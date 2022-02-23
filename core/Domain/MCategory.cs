06d8520e24688cc27e93ad6e2491713443da1ebc
ayrin <daraee.ayrin@gmail.com>
2022-02-23 11:57:02 +0330

    Required

--- core/Domain/MCategory.cs
+++ core/Domain/MCategory.cs
@@ -1,11 +1,16 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace core.Domain
 {
     public class MCategory
     {
         public int Id { get; set; }
+        [Display(Name = "نام دسته")]
+        [Required(ErrorMessage = "نباید بدون مفدار باشد")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "نباید بدون مفدار باشد")]
         public int ParentId { get; set; }
         public string Status { get; set; }
         
--- core/Domain/MProduct.cs
+++ core/Domain/MProduct.cs
@@ -1,16 +1,22 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace core.Domain
 {
     public class MProduct
     {
         public int Id { get; set; }
+
+        [Display(Name = "نام محصول")]
+        [Required(ErrorMessage = "نباید بدون مفدار باشد")]
         public string Name { get; set; }
         public double price { get; set; }
         public int Count { get; set; }
         public string discription { get; set; }
         public string MainImage { get; set; }
         public int IdMoreImage { get; set; }
+
+        [Required(ErrorMessage = "نباید بدون مفدار باشد")]
         public int IdCategory { get; set; }
         public bool Status { get; set; }
         public int IdColor { get; set; }
